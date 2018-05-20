b67a068cd9e4ef509a9121525653ec041f7db45f
白印潇 <baiyinxiao@baiyinxiaodemacbook-pro.local>
2018-05-20 12:08:04 +0800

    Initial Commit

---
+++ WisdomCompound_APP/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WisdomCompound_APP
+//
+//  Created by 白印潇 on 2018/5/20.
+//  Copyright © 2018年 白印潇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WisdomCompound_APPTests/WisdomCompound_APPTests.m
@@ -0,0 +1,39 @@
+//
+//  WisdomCompound_APPTests.m
+//  WisdomCompound_APPTests
+//
+//  Created by 白印潇 on 2018/5/20.
+//  Copyright © 2018年 白印潇. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface WisdomCompound_APPTests : XCTestCase
+
+@end
+
+@implementation WisdomCompound_APPTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
